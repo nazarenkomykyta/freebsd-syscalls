caa295c852bf05341bd38fcdb32e3fbb81c02800
Nazarenko Nikita Vladimirovich <nazarenko@tuta.com>
2025-07-16 20:04:00 +0200

    # Remove *mut c_void from types.rs ? 3156eb8

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,8 @@
-use core::ffi::c_void;
-
 pub type _CHAR = char;
 pub type _INT32 = i32;
 pub type _INT64 = i64;
 pub type _UINT32 = u32;
 pub type _UINT64 = i64;
-pub type _VOID =  *mut c_void;
 pub type _SIZET = usize;
 pub type _WORD = u16;
 pub type _DWORD = u32;
